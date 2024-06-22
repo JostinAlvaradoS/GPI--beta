3291ce73441b26d483a8dcd38a772a9142f51c86
Jostin <jealvarado2@utpl.edu.ec>
2024-06-21 22:41:48 -0500

    casi listo primer home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from "./auxiliares/header/header";
 import {FooterComponent} from "./auxiliares/footer/footer";
 import {SidebarComponent} from "./auxiliares/sidebar/sidebar";
+import {HomeComponent} from "./componentes/home/home";
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, FooterComponent, SidebarComponent
+    AppComponent, HeaderComponent, FooterComponent, SidebarComponent, HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
